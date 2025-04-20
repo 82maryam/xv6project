8482d8de2c6871e211c229205b2827def4130228
maryam masoomi <maryam345masoomi@gmail.com>
2025-04-20 18:10:23 +0330

    Update trigger_test.c

--- user/trigger_test.c
+++ user/trigger_test.c
@@ -1,11 +1,11 @@
-// user/trigger_test.c
-
+//اینجاد یک فایل برای این که تست کنیم سیستم کال ما درست کار می کنه 
 #include "kernel/types.h"
 #include "user/user.h"
 
 int main(void)
 {
     printf("trigger_test running!\n");
+   //تابع ترگیر فراخوانی می کنیم
     trigger();
     exit(0);
 }
